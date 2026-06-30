1acbd6514473038631b857ac2a1a2edf021e8f3e
Antoine PIETRI <antpietri@IUT.BX>
2026-06-30 16:57:09 +0200

    v1.1 - recherche, filtre operateur, impression, stats, etat machine, PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'reservation-gmp-v1.0';
+const ASSETS = ['/', '/index.html', '/petitgmp.png', '/BM_GMP_HD_1000.png', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      if (!r || r.status !== 200 || r.type !== 'basic') return r;
+      var rc = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, rc));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
